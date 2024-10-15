21a2bc4e74731b6ada933e93bba008b8064aeeb0
bromfi24 <838700@unizar.es>
2024-10-15 16:12:46 +0200

    Update supabaseClient.js

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv';
-dotenv.config();
 
 // Verificamos que las variables de entorno estén definidas
 const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-
