cf8d312c9cd8c54c22aae57e16088212e9294be4
josemifd <josemifd20@gmail.com>
2024-10-22 15:34:31 +0000

    Subo cosas para deplegar en Actions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EduPlanner/"
+  base: "https://unizar-30248-2024-eduplanner.github.io/Frontend/EduPlanner/"
 })
