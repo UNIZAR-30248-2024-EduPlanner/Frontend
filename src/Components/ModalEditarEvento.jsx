128d51f9daf96fb474079449a70183446ff658d6
Luis290602 <146435553+Luis290602@users.noreply.github.com>
2024-11-13 13:09:01 +0100

    Editar eventos personales, borrar eventos
    Me falta hacerlo dinámico, por ahora para ver los cambios hay que hacer f5

--- src/Components/ModalEditarEvento.jsx
+++ src/Components/ModalEditarEvento.jsx
@@ -2,14 +2,16 @@ import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from
 import { useAuth } from "../context/AuthContext";
 import { useState, useEffect } from "react";
 import ModalComponent from './ModalComponent';
+import { editCustomEvent } from '../supabase/customEvent/customEvent';
 
-const ModalEditarEvento = ({ isOpen, onOpenChange, title, date_start, date_finish, place, description }) => {
+const ModalEditarEvento = ({ isOpen, onOpenChange, title, date_start, date_finish, place, description, date, id }) => {
     const { user } = useAuth();
     const [nombreActividad, setNombreActividad] = useState("");
     const [horaInicio, setHoraInicio] = useState("");
     const [horaFin, setHoraFin] = useState("");
     const [espacioReservado, setEspacioReservado] = useState("");
     const [descripcion, setDescripcion] = useState("");
+    const [fecha, setFecha] = useState("");
     const [isConfirmModalOpen, setConfirmModalOpen] = useState(false); // Estado para el modal de confirmación
 
     useEffect(() => {
@@ -19,16 +21,21 @@ const ModalEditarEvento = ({ isOpen, onOpenChange, title, date_start, date_finis
             setHoraFin(date_finish);
             setEspacioReservado(place);
             setDescripcion(description);
+            setFecha(date);
         }
     }, [isOpen, title, date_start, date_finish, place, description]);
 
-    const modificarEvento = () => {
+    const modificarEvento = async () => {
         console.log("Nombre de la actividad:", nombreActividad);
         console.log("Hora de inicio:", horaInicio);
         console.log("Hora de finalización:", horaFin);
         console.log("Espacio reservado:", espacioReservado);
         console.log("Descripción:", descripcion);
+        console.log("ID del evento:", id);
+        console.log("Fecha:", fecha);
+        const updates = { name: nombreActividad.toString(), start_time: horaInicio, end_time: horaFin, /*place: espacioReservado.toString(),*/ description: descripcion.toString(), date: fecha };
         // Lógica para modificar el evento
+        await editCustomEvent(id, updates);
         setConfirmModalOpen(false); // Cierra el modal de confirmación
         onOpenChange(false); // Cierra el modal de edición
     };
@@ -56,7 +63,16 @@ const ModalEditarEvento = ({ isOpen, onOpenChange, title, date_start, date_finis
                                 </div>
 
                                 <div className="mb-4">
-                                    <h2 className="text-2xl font-bold">Hora</h2>
+                                    <h2 className="text-2xl font-bold">Fecha y Hora</h2>
+                                    <div>
+                                        <label className="block text-lg font-semibold">Fecha</label>
+                                        <input
+                                            type="date"
+                                            className="border p-2 mb-4"
+                                            value={fecha}
+                                            onChange={(e) => setFecha(e.target.value)}
+                                        />
+                                    </div>
                                     <div className="flex">
                                         <div className="mr-2">
                                             <label className="block text-lg font-semibold">Hora de inicio</label>
