e8129d17ee784f545c077aadb4eb8a33ef40f737
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2024-12-10 17:23:06 +0100

    E-4 and delete old test

--- src/Pages/Curso/CalendarioAsignaturaCrear.jsx
+++ src/Pages/Curso/CalendarioAsignaturaCrear.jsx
@@ -336,6 +336,7 @@ const CalendarioAsignaturaCrear = () => {
                 <div className="flecha">
                 <Tooltip content="Atrás">
                     <Button
+                    data-testid="save"
                     className="flecha-volver-container"
                     onClick={handleSave}
                     size="lg"
